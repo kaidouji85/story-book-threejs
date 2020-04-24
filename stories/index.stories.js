bce86bcf56bc03a0d3dbbcfc1d002c2b9a1e437e
takeuchi-y <takeuchi.y@opst.co.jp>
2020-04-24 15:02:18 +0900

    SimpleCubeのストーリーを追加した

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,18 +1,9 @@
 import {ThreeWorld} from "../src/three-world";
 import {SimpleSphere} from "../src/simple-sphere";
+import {SimpleCube} from "../src/simple-cube";
 
 export default {
-  title: 'Demo',
-};
-
-export const Heading = () => '<h1>Hello World</h1>';
-
-export const Button = () => {
-  const btn = document.createElement('button');
-  btn.type = 'button';
-  btn.innerText = 'Hello Button';
-  btn.addEventListener('click', e => console.log(e));
-  return btn;
+  title: 'three-js-objects',
 };
 
 export const Sphere = () => {
@@ -23,6 +14,18 @@ export const Sphere = () => {
 
   const sphere = new SimpleSphere();
   world.addGameObject(sphere);
+
+  return div;
+}
+
+export const Cube = () => {
+  const div = document.createElement('div');
+
+  const world = new ThreeWorld();
+  div.appendChild(world.getDomElement());
+
+  const Cube = new SimpleCube();
+  world.addGameObject(Cube);
 
   return div;
 }
