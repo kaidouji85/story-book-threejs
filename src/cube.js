baabfed7231d4e832c2e7b5ea6a77d3b6c9b54b2
takeuchi-y <takeuchi.y@opst.co.jp>
2020-04-24 14:19:59 +0900

    Cubeを別オブジェクトに分けた

---
+++ src/cube.js
@@ -0,0 +1,31 @@
+import * as THREE from "three";
+
+/**
+ * シンプルなキューブ
+ */
+export class SimpleCube {
+  constructor() {
+    this.geometry = new THREE.BoxGeometry();
+    this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+    this.cube = new THREE.Mesh(this.geometry, this.material);
+  }
+
+  /**
+   * シーンに追加するオブジェクトを取得する
+   *
+   * @return {THREE.Object3D}
+   */
+  getObject3D() {
+    return this.cube;
+  }
+
+  /**
+   * requestAnimationFrameの処理
+   *
+   * @return void
+   */
+  animate() {
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+  }
+}
--- src/three-main.js
+++ src/three-main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {SimpleCube} from "./cube";
 
 /**
  * three.js メイン処理
@@ -6,17 +7,16 @@ import * as THREE from "three";
 export class ThreeMain {
   constructor() {
     this.scene = new THREE.Scene();
+
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    this.camera.position.z = 5;
 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(window.innerWidth, window.innerHeight);
 
-    this.geometry = new THREE.BoxGeometry();
-    this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-    this.cube = new THREE.Mesh(this.geometry, this.material);
-    this.scene.add(this.cube);
+    this.cube = new SimpleCube();
+    this.scene.add(this.cube.getObject3D());
 
-    this.camera.position.z = 5;
     this.animate();
   }
 
@@ -27,8 +27,7 @@ export class ThreeMain {
    */
   animate() {
     requestAnimationFrame(this.animate.bind(this));
-    this.cube.rotation.x += 0.01;
-    this.cube.rotation.y += 0.01;
+    this.cube.animate();
     this.renderer.render(this.scene, this.camera);
   }
 
