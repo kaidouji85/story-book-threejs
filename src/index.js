013000117a512ea108d7a98f8c9fac08b7ec7686
takeuchi-y <takeuchi.y@opst.co.jp>
2020-04-24 14:05:59 +0900

    three.jsの処理をクラスとして切り出した

--- src/index.js
+++ src/index.js
@@ -1,28 +1,36 @@
 import * as THREE from 'three';
 
 window.addEventListener('load', () => {
-  var scene = new THREE.Scene();
-  var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  document.body.appendChild( renderer.domElement );
-
-  var geometry = new THREE.BoxGeometry();
-  var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-  var cube = new THREE.Mesh( geometry, material );
-  scene.add( cube );
-
-  camera.position.z = 5;
-
-  var animate = function () {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-  };
-
-  animate();
+  const threeMain = new ThreeMain();
+  const threeDomElement = threeMain.getDomElement();
+  document.body.append(threeDomElement);
 });
+
+export class ThreeMain {
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+    this.geometry = new THREE.BoxGeometry();
+    this.material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    this.cube = new THREE.Mesh( this.geometry, this.material );
+    this.scene.add( this.cube );
+
+    this.camera.position.z = 5;
+    this.animate();
+  }
+
+  animate() {
+    requestAnimationFrame(this.animate.bind(this));
+    this.cube.rotation.x += 0.01;
+    this.cube.rotation.y += 0.01;
+    this.renderer.render(this.scene, this.camera);
+  }
+
+  getDomElement() {
+    return this.renderer.domElement;
+  }
+}
